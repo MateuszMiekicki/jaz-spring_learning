be73f69b912e7a8e383637bed4d8b71b641f9a9d
Mateusz Miękicki <miekickimateusz@gmail.com>
2020-10-29 21:37:16 +0100

    code refactoring according to the recommendations from the file 'task 1 - configuration and average'

--- app/src/main/java/pl/edu/pjwstk/jaz/average/AverageController.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/average/AverageController.java
@@ -11,13 +11,22 @@ import java.util.List;
 public class AverageController {
 
     @GetMapping("average")
-    public String getAverage(@RequestParam("numbers") String numbers) {
+    public String getAverage(@RequestParam(value = "numbers", required = false) String numbers) {
+        if (numbers.isEmpty()) {
+            return "Please put parameters.";
+        }
         List<String> items = Arrays.asList(numbers.split("\\s*,\\s*"));
         int sumOfAllNumbers = 0;
-        for (int i = 0; i < items.size(); ++i) {
-            sumOfAllNumbers += Integer.parseInt(items.get(i));
+        for (String item : items) {
+            sumOfAllNumbers += Integer.parseInt(item);
         }
-        double avg = (double)sumOfAllNumbers / (double)items.size();
-        return String.valueOf(avg);
+        double avg = (double) sumOfAllNumbers / (double) items.size();
+        avg = (int)(avg * 100.0) / 100.0;
+        if (avg == (int) avg) {
+            return "Average equals: " + (int) avg;
+        }
+        else {
+            return "Average equals: " + avg;
+        }
     }
 }
--- app/src/test/java/pl/edu/pjwstk/jaz/average/AverageControllerTestIT.java
+++ app/src/test/java/pl/edu/pjwstk/jaz/average/AverageControllerTestIT.java
@@ -20,6 +20,50 @@ public class AverageControllerTestIT {
                 .get("/api/average")
                 .then()
                 .statusCode(200)
-                .body(equalTo("2.5"));
+                .body(equalTo("Average equals: 2.5"));
+    }
+
+    @Test
+    public void should_calculate_simple_integer_average() {
+        var response = given()
+                .param("numbers", "4,3,1,7,5")
+                .when()
+                .get("/api/average")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Average equals: 4"));
+    }
+
+    @Test
+    public void should_calculate_simple_measurable_average() {
+        var response = given()
+                .param("numbers", "2,1")
+                .when()
+                .get("/api/average")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Average equals: 1.5"));
+    }
+
+    @Test
+    public void should_calculate_simple_periodic_average() {
+        var response = given()
+                .param("numbers", "2,1,1")
+                .when()
+                .get("/api/average")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Average equals: 1.33"));
+    }
+
+    @Test
+    public void should_inform_about_lack_of_parameters() {
+        var response = given()
+                .param("numbers", "")
+                .when()
+                .get("/api/average")
+                .then()
+                .statusCode(200)
+                .body(equalTo("Please put parameters."));
     }
 }
