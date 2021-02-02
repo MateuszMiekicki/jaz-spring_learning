0fd4fe84c6f57300ba589d6521c5a007f0e728a8
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-02-02 19:52:34 +0100

    Pre-filtering of requests has been added.
    Pre-filtering of requests has been added.
    Removed redundant package from user repository.

--- app/src/main/java/pl/edu/pjwstk/jaz/repository/UserRepository.java
+++ app/src/main/java/pl/edu/pjwstk/jaz/repository/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.edu.pjwstk.jaz.entity.UserEntity;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
---
+++ app/src/main/java/pl/edu/pjwstk/jaz/security/SecurityConfigure.java
@@ -0,0 +1,19 @@
+package pl.edu.pjwstk.jaz.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
